6c252f46396b954a67ba56955cf78973fc587353
kiarashashoori <ashoorikiarash8585@gmail.com>
2026-01-06 00:20:27 +0330

    add auriga image

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.image import Image
+from kivy.core.window import Window
 
 import os
 
 class app(App):
+    def on_start(self):
+        Window.size = (1100, 600)
+        Window.minimum_width = 800
+        Window.minimum_height = 600
+
     def build(self):
         options = ['increase brightness', 'decrease brightness' ,'increase contrast',
                    'decrease contrast','decrease saturation' , 'increase saturation' ,
@@ -33,10 +40,14 @@ class app(App):
         confirm_btn = Button(text='start',size_hint = (None,None),size = ("75dp","40dp"),background_normal='',background_color=(0,0.8,0.3,1))
         confirm_layout.add_widget(confirm_btn)
 
+        image_layout = AnchorLayout(anchor_x='right', anchor_y='bottom')
+        auriga_image = Image(source = 'Auriga.png',size_hint = (None,None))
+        image_layout.add_widget(auriga_image)
 
         screen_layout = FloatLayout()
         screen_layout.add_widget(confirm_layout)
         screen_layout.add_widget(checkbox_layout)
+        screen_layout.add_widget(image_layout)
         return screen_layout
 
 if __name__ == '__main__':
